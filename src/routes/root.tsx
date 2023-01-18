0f44a8c2be468529381bd987f3e3473555349d63
PabloVegaValverdeUA <pvegav@autonoma.edu.pe>
2023-01-17 23:37:59 -0500

    [ADD] Tailwind CSS + Components

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,23 +1,91 @@
-import logo from "../logo.svg";
+import { Tarjeta, TarjetaExpo } from "../components/card";
+import { Container } from "../components/container/container";
+import Header from "../components/header/header";
+import { Me } from "../store/me";
 
 const Root = () => {
+  const { profile, who } = Me;
+  const CardsLeftTop = [
+    {
+      title: "Quien soy",
+      content: [who.main],
+    },
+    {
+      title: "Contactame",
+      content: [
+        "Lima, Perú",
+        '<a className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"' +
+          'href="mailto:' +
+          profile.emailCurrent +
+          '?subject=Contactar a Pablo Vega - DESARROLLADOR" target="_blank">' +
+          profile.emailCurrent +
+          "</a>",
+        '<a className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"' +
+          ' href="https://wa.me/' +
+          profile.whatsapp +
+          '/" target="_blank">' +
+          profile.phoneCurrent +
+          "</a>",
+      ],
+    },
+    {
+      title: "Servicios",
+      content: [
+        "Diseño Web Responsive",
+        "Desarrollo Backend Microservicios",
+        "Desarrollo FrontEnd Responsive",
+      ],
+    },
+  ];
+  const CardsRightDown = [
+    {
+      title: "Años de experiencia",
+      content: [profile.yearsExperience],
+    },
+    { title: "Proyectos", content: [profile.proyects] },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <div className="mx-auto text-center font-serif p-3 md:p-7">
+          <h1 className="text-2xl md:text-5xl">{profile.authorMYS}</h1>
+          <h1 className="text-2xl md:text-5xl">{profile.mainDescription}</h1>
+          <h1 className="text-2xl md:text-5xl">{profile.subDescription}</h1>
+        </div>
+        <div className="flex flex-col md:flex-row gap-5 font-serif p-10 md:py-20 md:px-16">
+          {/* CARDS LEFT MD:TOP */}
+          <div className={"flex flex-1 flex-col gap-12 my-auto"}>
+            {CardsLeftTop.map((card, i) => (
+              <Tarjeta key={i} title={card.title} content={card.content} />
+            ))}
+          </div>
+          {/* IMAGE PROFILE */}
+          <div className="hidden md:block border-2 border-gray-200 rounded-t-full rounded-b-full p-4 mx-5">
+            <img
+              src="https://picsum.photos/400/800"
+              className={"rounded-t-full rounded-b-full w-full h-full"}
+              alt="Pablo Vega"
+            />
+          </div>
+          {/* CARDS RIGHT MD:DOWN */}
+          <div className={"flex flex-1 flex-col gap-12 my-auto"}>
+            {CardsRightDown.map((card, i) => (
+              <TarjetaExpo key={i} title={card.title} content={card.content} />
+            ))}
+          </div>
+          {/* IMAGE PROFILE */}
+          <div className="md:hidden mx-auto border-2 border-gray-200 rounded-t-full rounded-b-full p-4">
+            <img
+              src="https://picsum.photos/400/800"
+              className={"rounded-t-full rounded-b-full w-full h-full"}
+              alt="Pablo Vega"
+            />
+          </div>
+        </div>
+      </Container>
+      );
+    </>
   );
 };
 
---
+++ src/store/me.tsx
@@ -0,0 +1,23 @@
+export const Me = {
+  profile: {
+    author: "Pablo Vega",
+    authorMYS: "PABLO VEGA",
+    name: "Pablo Cesar Vega Valverde",
+    acronym: "PCVV",
+    firstName: "Pablo Cesar",
+    lastName: "Vega Valverde",
+    description: "Desarrollador Backend - Frontend",
+    mainDescription: "DESARROLLADOR",
+    subDescription: "BACKEND - FRONTEND",
+    emailCurrent: "pvegav@autonoma.edu.pe",
+    emailUniversity: "pvegav@autonoma.edu.pe",
+    emailAAT: "pvegav@aat.com.pe",
+    phoneCurrent: "+51 980251512",
+    whatsapp: "51980251512",
+    yearsExperience: "1",
+    proyects: "2",
+  },
+  who: {
+    main: "Compañero de trabajo confliable, adaptable a cualquier tipo de proyecto, con ganas de aprender y mejorar cada día.",
+  },
+};
