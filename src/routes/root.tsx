3461033457f050b35a2eb887330900a6490dfd20
Pablo Vega <pcvv@aat.com.pe>
2023-01-19 09:35:48 -0500

    [UPDATE] Quitar <title> root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -46,7 +46,6 @@ const Root = () => {
   return (
     <>
       <Helmet>
-        <title>{profile.acronymDescrip}</title>
         <meta
           name="description"
           content="Este sitio web está hecho para mostrar el recorrido, conocimientos y herramientas que he adquirido a lo largo de mi vida como estudiante de la carrera de Ingeniería en Sistemas"
