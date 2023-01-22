2da3c176c757604c8aedf5c2a1f89e0a53ef6d6c
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2023-01-22 18:56:21 -0500

    [UPDATE] Header actualizado responsive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Paths, PathSociales } from "../../store";
 import { DropdownLanguage } from "../dropdown/dropchangelanguage";
 import { Github, Facebook, LinkedIn } from "../icons";
-
+import { AiOutlineMenu } from "react-icons/ai";
 const Header = () => {
   const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const menuLinks = useRef<HTMLDivElement>(null);
+  const activeLink = "text-yellow-400";
+  const inactiveLink = "text-red-900";
+  document.body.classList.add("overflow-auto");
+
+  useEffect(() => {
+    isOpen
+      ? document.body.classList.replace("overflow-auto", "overflow-hidden")
+      : document.body.classList.replace("overflow-hidden", "overflow-auto");
+  }, [isOpen]);
+
   type Link = {
     path: string;
     title: string;
@@ -17,18 +29,18 @@ const Header = () => {
     {
       path: Paths.tools,
       title: t("header.dev_tools_short"),
-      arialabel: "Read more about my dev tools.",
+      arialabel: "Read more about my dev tools."
     },
     {
       path: Paths.tools,
       title: t("header.contact"),
-      arialabel: "Contact with me.",
-    },
+      arialabel: "Contact with me."
+    }
   ];
 
   type Network = {
     path: string;
-    icon: React.ReactNode;
+    icon: ReactNode;
     title: string;
     arialabel: string;
   };
@@ -38,37 +50,42 @@ const Header = () => {
       path: PathSociales.github,
       icon: <Github />,
       title: "Facebook",
-      arialabel: "Redirect to my  GitHub.",
+      arialabel: "Redirect to my  GitHub."
     },
     {
       path: PathSociales.facebook,
       icon: <Facebook />,
       title: "Facebook",
-      arialabel: "Redirect to facebook.",
+      arialabel: "Redirect to facebook."
     },
     {
       path: PathSociales.linkedin,
       icon: <LinkedIn />,
       title: "LinkedIn",
-      arialabel: "Redirect to my LinkedIn.",
-    },
+      arialabel: "Redirect to my LinkedIn."
+    }
   ];
 
   return (
-    <header className="relative bg-white">
+    <header
+      className={
+        "bg-white uppercase font-semibold lg:text-lg" +
+        (isOpen ? "h-screen sm:sm:h-auto" : "")
+      }>
       <div className="container mx-auto px-4 py-1 md:py-3 flex items-center justify-between">
         {/* ROUTES */}
         <div className="hidden sm:flex flex-1 justify-start">
           <ul className="flex flex-row gap-3 items-center">
             {links.map((link, i) => (
               <li key={i} className="text-center">
-                <Link
+                <NavLink
                   to={link.path}
-                  className="uppercase text-xs sm:text-sm md:text-base font-bold"
-                  aria-label={link.arialabel}
-                >
+                  className={({ isActive }) =>
+                    isActive ? activeLink : inactiveLink
+                  }
+                  aria-label={link.arialabel}>
                   {link.title}
-                </Link>
+                </NavLink>
               </li>
             ))}
           </ul>
@@ -78,13 +95,19 @@ const Header = () => {
           <Link
             to={Paths.root}
             aria-label="My universitary's Logo, click to return to the init."
-          >
+            onClick={() => setIsOpen(false)}>
             <img
               src="./imgs/Logo.png"
               alt="Logo"
               className="h-14 sm:h-16 md:h-20"
             />
           </Link>
+          {/* MENU LINKS */}
+          <div className="block sm:hidden self-center ml-5" ref={menuLinks}>
+            <button onClick={() => setIsOpen(!isOpen)}>
+              <AiOutlineMenu />
+            </button>
+          </div>
         </div>
         {/* SOCIAL NETWORKS */}
         <div className="flex flex-1 justify-end">
@@ -98,8 +121,7 @@ const Header = () => {
                   href={network.path}
                   target="_blank"
                   rel="noreferrer"
-                  aria-label={network.arialabel}
-                >
+                  aria-label={network.arialabel}>
                   {network.icon}
                 </a>
               </li>
@@ -107,6 +129,28 @@ const Header = () => {
           </ul>
         </div>
       </div>
+      {/* MENU ROUTES */}
+      {isOpen && (
+        <div className="sm:hidden py-4 h-screen" ref={menuLinks}>
+          <ul className="flex flex-col gap-3 items-center text-sm">
+            {links.map((link, i) => (
+              <li
+                key={i}
+                className="text-center"
+                onClick={() => setIsOpen(false)}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    isActive ? activeLink : inactiveLink
+                  }
+                  aria-label={link.arialabel}>
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: { emerald: colors.emerald, rose: colors.rose, cyan: colors.cyan },
-    },
+      colors: {
+        emerald: colors.emerald,
+        amber: colors.amber,
+        rose: colors.rose,
+        cyan: colors.cyan,
+        yellow: colors.yellow
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
