653c54416e7cdc122155eff44253d5b2cb32fef6
Pablo Vega <pcvv@aat.com.pe>
2023-01-26 09:30:09 -0500

    [ADD] SendEmail y SendWhatsapp componets

--- src/components/profile/sendWhatsapp.tsx
+++ src/components/profile/sendWhatsapp.tsx
@@ -1,14 +1,23 @@
-export const SendWhatsapp = () => {
+import { useEffect, useState } from "react";
+import { TypeProfile } from "../../types";
+
+export const SendWhatsapp = (profile: TypeProfile) => {
+  const { whatsapp, phoneCurrent } = profile;
+  let [link, setLink] = useState(`https://wa.me/${whatsapp}`);
+
+  useEffect(() => {
+    if (window.innerWidth > 899) {
+      setLink(`https://web.whatsapp.com/send/?phone=${whatsapp}`);
+    }
+  }, [whatsapp]);
+
   return (
-    <>
-      Número de celular
-      <a
-        className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"
-        href="https://wa.me/51980251512/"
-        rel="noopener"
-      >
-        +51 980251512
-      </a>
-    </>
+    <a
+      className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"
+      href={link}
+      target="_blank"
+      rel="noreferrer">
+      {phoneCurrent}
+    </a>
   );
 };
