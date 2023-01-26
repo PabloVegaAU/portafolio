ec6b26c25fa767f32180c02b60b1bc7e67be2a1f
Pablo Vega <pcvv@aat.com.pe>
2023-01-26 09:29:12 -0500

    [ADD] Se agregó TypeMe

---
+++ src/types/index.tsx
@@ -0,0 +1,1 @@
+export type { TypeMe, TypeProfile, TypeWho } from "./me";
---
+++ src/types/me.tsx
@@ -0,0 +1,28 @@
+export type TypeMe = {
+  profile: TypeProfile;
+  who: TypeWho;
+};
+
+export type TypeProfile = {
+  author: string;
+  authorMYS: string;
+  name: string;
+  acronymName: string;
+  acronymDescrip: string;
+  firstName: string;
+  lastName: string;
+  description: string;
+  mainDescription: string;
+  subDescription: string;
+  emailCurrent: string;
+  emailUniversity: string;
+  emailAAT: string;
+  phoneCurrent: string;
+  whatsapp: string;
+  yearsExperience: string;
+  proyects: string;
+};
+
+export type TypeWho = {
+  main: string;
+};
