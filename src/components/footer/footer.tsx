31bbfdeb969f871b9a94d8e4d58f701141007593
Pablo Vega <pcvv@aat.com.pe>
2023-02-01 09:08:23 -0500

    [UPDATE] Agregar Logos en footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,26 @@
 export const Footer = () => {
-  return <>123123</>;
+	return (
+		<div className="flex flex-row gap-2 m-2.5 justify-center">
+			<img
+				src="./imgs/footer/UniversidadAutonomaPeruLogo.png"
+				className={"rounded-full w-12 h-12 object-cover"}
+				alt="Pablo Vega"
+			/>
+			<img
+				src="./imgs/footer/Scrum-Foundation-Professional-Certificate.png"
+				className={"rounded-full w-12 h-12 object-cover"}
+				alt="Pablo Vega"
+			/>
+			<img
+				src="./imgs/footer/AATLogo.png"
+				className={"rounded-full w-12 h-12 object-cover"}
+				alt="Pablo Vega"
+			/>
+			<img
+				src="./imgs/footer/PolinsumosLogo.png"
+				className={"rounded-full w-12 h-12 object-cover"}
+				alt="Pablo Vega"
+			/>
+		</div>
+	);
 };
