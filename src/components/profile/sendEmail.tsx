accfd0c8fb83dcde35ba30145fb39d3e9592be09
Pablo Vega <pcvv@aat.com.pe>
2023-02-01 09:09:21 -0500

    [UPDATE] Aplicar format ROME

--- src/components/profile/sendEmail.tsx
+++ src/components/profile/sendEmail.tsx
@@ -1,17 +1,14 @@
 import { TypeProfile } from "../../types";
 
 export const SendEmail = (profile: TypeProfile) => {
-  return (
-    <a
-      className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"
-      href={
-        "mailto:" +
-        profile.emailCurrent +
-        "?subject=Contactar a Pablo Vega - DESARROLLADOR"
-      }
-      target="_blank"
-      rel="noreferrer">
-      {profile.emailCurrent}
-    </a>
-  );
+	return (
+		<a
+			className="text-red-500 hover:text-red-600 font-mono font-medium"
+			href={`mailto:${profile.emailCurrent}?subject=Contactar a Pablo Vega - DESARROLLADOR`}
+			target="_blank"
+			rel="noreferrer"
+		>
+			{profile.emailCurrent}
+		</a>
+	);
 };
--- src/components/profile/sendWhatsapp.tsx
+++ src/components/profile/sendWhatsapp.tsx
@@ -2,22 +2,23 @@ import { useEffect, useState } from "react";
 import { TypeProfile } from "../../types";
 
 export const SendWhatsapp = (profile: TypeProfile) => {
-  const { whatsapp, phoneCurrent } = profile;
-  let [link, setLink] = useState(`https://wa.me/${whatsapp}`);
+	const { whatsapp, phoneCurrent } = profile;
+	let [link, setLink] = useState(`https://wa.me/${whatsapp}`);
 
-  useEffect(() => {
-    if (window.innerWidth > 899) {
-      setLink(`https://web.whatsapp.com/send/?phone=${whatsapp}`);
-    }
-  }, [whatsapp]);
+	useEffect(() => {
+		if (window.innerWidth > 899) {
+			setLink(`https://web.whatsapp.com/send/?phone=${whatsapp}`);
+		}
+	}, [whatsapp]);
 
-  return (
-    <a
-      className="text-emerald-500 hover:text-emerald-600 font-mono font-medium"
-      href={link}
-      target="_blank"
-      rel="noreferrer">
-      {phoneCurrent}
-    </a>
-  );
+	return (
+		<a
+			className="text-red-500 hover:text-red-600 font-mono font-medium"
+			href={link}
+			target="_blank"
+			rel="noreferrer"
+		>
+			{phoneCurrent}
+		</a>
+	);
 };
