b4d08cc701d783f67e432cb9e76a3959c2e2c504
Pablo Vega <pcvv@aat.com.pe>
2023-02-01 09:08:06 -0500

    [UPDATE] Aplicar format ROME

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 type ContainerProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 export const Container = (props: ContainerProps) => {
-  return (
-    <div className="container mx-auto px-2">
-      <hr />
-      <div>{props.children}</div>
-    </div>
-  );
+	return (
+		<div className="container mx-auto px-2">
+			<hr />
+			{props.children}
+		</div>
+	);
 };
