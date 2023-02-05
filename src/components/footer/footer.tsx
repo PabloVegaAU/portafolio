b170cbf4ddae9e0da4588d42f08fca7a55d16dd5
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2023-02-04 23:49:55 -0500

    [UPDATE] Aplicar footer container

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,26 +1,30 @@
+import { FooterContainer } from "../container/footerContainer";
+
 export const Footer = () => {
 	return (
-		<div className="flex flex-row gap-2 py-4 justify-center">
-			<img
-				src="./imgs/footer/UniversidadAutonomaPeruLogo.png"
-				className={"rounded-full w-12 h-12 object-cover"}
-				alt="Pablo Vega"
-			/>
-			<img
-				src="./imgs/footer/Scrum-Foundation-Professional-Certificate.png"
-				className={"rounded-full w-12 h-12 object-cover"}
-				alt="Pablo Vega"
-			/>
-			<img
-				src="./imgs/footer/AATLogo.png"
-				className={"rounded-full w-12 h-12 object-cover"}
-				alt="Pablo Vega"
-			/>
-			<img
-				src="./imgs/footer/PolinsumosLogo.png"
-				className={"rounded-full w-12 h-12 object-cover"}
-				alt="Pablo Vega"
-			/>
-		</div>
+		<FooterContainer>
+			<div className="flex flex-row gap-2 py-2 justify-center">
+				<img
+					src="./imgs/footer/UniversidadAutonomaPeruLogo.png"
+					className={"rounded-full w-12 h-12 object-cover"}
+					alt="Pablo Vega"
+				/>
+				<img
+					src="./imgs/footer/Scrum-Foundation-Professional-Certificate.png"
+					className={"rounded-full w-12 h-12 object-cover"}
+					alt="Pablo Vega"
+				/>
+				<img
+					src="./imgs/footer/AATLogo.png"
+					className={"rounded-full w-12 h-12 object-cover"}
+					alt="Pablo Vega"
+				/>
+				<img
+					src="./imgs/footer/PolinsumosLogo.png"
+					className={"rounded-full w-12 h-12 object-cover"}
+					alt="Pablo Vega"
+				/>
+			</div>
+		</FooterContainer>
 	);
 };
